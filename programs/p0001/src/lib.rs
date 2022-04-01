0b47b40064b6569ad5ea1c203a5f5e050334dc46
Kunkka0822 <101172481+Kunkka0822@users.noreply.github.com>
2022-04-01 06:19:40 -0700

    wip

--- programs/p0001/src/lib.rs
+++ programs/p0001/src/lib.rs
@@ -5,11 +5,35 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod p0001 {
     use super::*;
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
         Ok(())
     }
 }
 
+#derive[(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer=user, space=8 + 8)]
+    pub base_account: Account<'info BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub count: u64;
+}
