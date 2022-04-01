7020f94ed36008a8961e0c23171c9a12db63e640
Kunkka <hungryAdmiral0512@gmail.com>
2022-04-01 23:30:04 +0800

    wip

--- programs/p0001/src/lib.rs
+++ programs/p0001/src/lib.rs
@@ -1,27 +1,27 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2DhNq3n17844BkNDngD9iDgMPwLYqUxo4VcnWjLUry6E");
 
 #[program]
 pub mod p0001 {
     use super::*;
-    pub fn create(ctx: Context<Create>) -> ProgramResult {
+    pub fn create(ctx: Context<Create>) -> Result<()> {
         let base_account = &mut ctx.accounts.base_account;
         base_account.count = 0;
         Ok(())
     }
 
-    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
         let base_account = &mut ctx.accounts.base_account;
         base_account.count += 1;
         Ok(())
     }
 }
 
-#derive[(Accounts)]
+#[derive(Accounts)]
 pub struct Create<'info> {
     #[account(init, payer=user, space=8 + 8)]
-    pub base_account: Account<'info BaseAccount>,
+    pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -35,5 +35,5 @@ pub struct Increment<'info> {
 
 #[account]
 pub struct BaseAccount {
-    pub count: u64;
+    pub count: u64,
 }
